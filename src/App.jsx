206668a9492a1baccac6023a2c18e4fa146b8afb
Ugo Chinememerem <ugofranklin22@gmail.com>
2023-10-29 17:02:11 +0100

    programpage

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import WhatWeOffer from "./pages/Copoerate service/WhatWeOffer";
 import University from "./pages/university/University";
 import Startup from "./pages/Startup/Startup";
 import Talent from "./pages/Talent/Talent";
+import Program from "./pages/Programs/Program";
 const App = () => {
   return (
     <Router>
@@ -16,6 +17,7 @@ const App = () => {
         <Route path="/university" element={<University />} />
         <Route path="/startup" element={<Startup />} />
         <Route path="/talent" element={<Talent />} />
+        <Route path="/program" element={<Program />} />
       </Routes>
     </Router>
   );
