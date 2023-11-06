cc0aebdde551ed5fc687d163592eecd3a5b77ba4
Ugo Chinememerem <ugofranklin22@gmail.com>
2023-11-06 02:20:05 +0100

    research

--- src/pages/university/University.jsx
+++ src/pages/university/University.jsx
@@ -77,33 +77,13 @@ const University = () => {
                     the right university partners, ensuring alignment with your
                     digital innovation goals.
                   </p>
-                  {/* <a
-                    href="#"
-                    className="icon-link gap-1 icon-link-hover stretched-link"
-                  >
-                    Continue reading
-                    <svg className="bi">
-                      <use xlinkHref="#chevron-right"></use>
-                    </svg>
-                  </a> */}
                 </div>
                 <div className="col-auto d-none d-lg-block">
-                  <svg
-                    className="bd-placeholder-img"
-                    width="200"
-                    height="250"
-                    xmlns="http://www.w3.org/2000/svg"
-                    role="img"
-                    aria-label="Placeholder: Thumbnail"
-                    preserveAspectRatio="xMidYMid slice"
-                    focusable="false"
-                  >
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#55595c"></rect>
-                    <text x="50%" y="50%" fill="#eceeef" dy=".3em">
-                      Thumbnail
-                    </text>
-                  </svg>
+                  <img
+                    width={260}
+                    height={270}
+                    src="https://res.cloudinary.com/dtfvdjvyr/image/upload/v1699233455/myriam-jessier-eveI7MOcSmw-unsplash_knjqjq.jpg"
+                  />
                 </div>
               </div>
             </div>
