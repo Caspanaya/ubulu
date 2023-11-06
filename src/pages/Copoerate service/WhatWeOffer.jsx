8f33141861182d69f42e2062c17366532b1dbb80
Ugo Chinememerem <ugofranklin22@gmail.com>
2023-11-06 01:22:27 +0100

    img:whatweoffer

--- src/pages/Copoerate service/WhatWeOffer.jsx
+++ src/pages/Copoerate service/WhatWeOffer.jsx
@@ -21,18 +21,22 @@ const WhatWeOffer = () => {
     {
       title: "Growth ambition and goals",
       text: "We transform inspiring ambitions into practical goals that resonate with both visionaries and pragmatists within and outside your organization.",
+      img: "https://res.cloudinary.com/dtfvdjvyr/image/upload/v1699223453/paolo-margari-KVYPpbpji4E-unsplash_sjkeqi.jpg", // Add the URL for the image for this card
     },
     {
       title: "Growth opportunities ",
       text: "We challenge existing myths, uncover growth opportunities, and assess your organization's readiness to seize them.",
+      img: "https://res.cloudinary.com/dtfvdjvyr/image/upload/v1699228004/javier-allegue-barros-C7B-ExXpOIE-unsplash_yd1t4j.jpg", // Add the URL for the image for this card
     },
     {
       title: "Growth execution",
       text: "In this phase, we translate growth engines and enablers into actionable plans and effective change management. We leverage both rational and emotional drivers to foster wholehearted commitment across your organization, ensuring the successful execution of your growth strategy.",
+      img: "https://res.cloudinary.com/dtfvdjvyr/image/upload/v1699228173/cytonn-photography-n95VMLxqM2I-unsplash_fke0zy.jpg", // Add the URL for the image for this card
     },
     {
       title: "Growth strategy",
       text: "We work with you to define the engines of your growth, such as new market segments, innovative propositions, novel market channels, and potential acquisitions. Additionally, we address internal growth enablers like competencies, systems, and processes.",
+      img: "https://res.cloudinary.com/dtfvdjvyr/image/upload/v1699228456/kaleidico-26MJGnCM0Wc-unsplash_qpz8jq.jpg", // Add the URL for the image for this card
     },
     // Add more card data objects as needed
   ];
@@ -40,14 +44,17 @@ const WhatWeOffer = () => {
     {
       title: "Innovation North Star",
       text: "Do you have an inspiring vision for your innovation efforts? If you don’t know where you are going, how can you be sure to be successful? We can help you link your business goals to an effective innovation strategy.",
+      img: "https://res.cloudinary.com/dtfvdjvyr/image/upload/v1699228573/ramon-salinero-vEE00Hx5d0Q-unsplash_boz3h8.jpg", // Add the URL for the image for this card
     },
     {
       title: "Innovation Roadmap ",
       text: "We assist in transforming your Innovation North Star into a comprehensive innovation roadmap. This roadmap guides your journey towards sustainable, innovative growth over time, and it helps you build a portfolio that aligns with your innovation goals.",
+      img: "https://res.cloudinary.com/dtfvdjvyr/image/upload/v1699228857/matt-duncan-IUY_3DvM__w-unsplash_glt9tz.jpg", // Add the URL for the image for this card
     },
     {
       title: "Strategy Execution",
       text: "In today's landscape, strategy and execution are inseparable. The urgency brought about by digital disruptions emphasizes the importance of effective execution.Do you have the skills and competencies to deliver on your strategy?",
+      img: "https://res.cloudinary.com/dtfvdjvyr/image/upload/v1699228956/patrick-perkins-ETRPjvb0KM0-unsplash_1_goswwg.jpg", // Add the URL for the image for this card
     },
 
     // Add more card data objects as needed
@@ -56,18 +63,22 @@ const WhatWeOffer = () => {
     {
       title: "What’s your offering?",
       text: "Your offering goes beyond being merely your product or core process. It encompasses more than just a comprehensive set of product or service features and benefits. It involves creating a user and buyer experience that optimally aligns with their rational and emotional needs.",
+      img: "https://res.cloudinary.com/dtfvdjvyr/image/upload/v1699229169/microsoft-365-TLiWhlDEJwA-unsplash_qdznje.jpg",
     },
     {
       title: "Who is your client? ",
       text: "It all begins with the realization of who your true client is and understanding their genuine needs. We guide you through the intricacies of today's markets, value chains, buying powers, and processes, helping you discover the often latent needs of your actual buyers and users.",
+      img: "https://res.cloudinary.com/dtfvdjvyr/image/upload/v1699229735/docusign-AKKhZqyLeJ4-unsplash_khesbr.jpg",
     },
     {
       title: "Organisational Enhancement ",
       text: "Enhancing your go-to-market strategy significantly involves making improvements in marketing, sales, and other front-end disciplines within your organization. These are not the easiest areas to change. We leverage all available avenues for change, a matter of critical importance in achieving successful implementation.",
+      img: "https://res.cloudinary.com/dtfvdjvyr/image/upload/v1699229933/paico-oficial-ZDYN7azwO6c-unsplash_fqskz4.jpg",
     },
     {
       title: " Integration ",
       text: "Establishing and maintaining a business relationship is a complex and time-consuming psychological process. We create a customer journey roadmap that outlines interactions between your client and your offering, deploying the most effective channels and triggering influential mental processes.",
+      img: "https://res.cloudinary.com/dtfvdjvyr/image/upload/v1699230079/nasa-Q1p7bh3SHj8-unsplash_izzikl.jpg",
     },
 
     // Add more card data objects as needed
@@ -119,22 +130,12 @@ const WhatWeOffer = () => {
             {cardData.map((card, index) => (
               <div className="col" key={index}>
                 <div className="card shadow-sm">
-                  <svg
-                    className="bd-placeholder-img card-img-top"
-                    width="100%"
-                    height="225"
-                    xmlns="http://www.w3.org/2000/svg"
-                    role="img"
-                    aria-label="Placeholder: Thumbnail"
-                    preserveAspectRatio="xMidYMid slice"
-                    focusable="false"
-                  >
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#55595c"></rect>
-                    <text x="50%" y="50%" fill="#eceeef" dy=".3em">
-                      Thumbnail
-                    </text>
-                  </svg>
+                  <img
+                    width={360}
+                    height={225}
+                    src={card.img}
+                    alt={card.title}
+                  />
                   <div className="card-body">
                     <h1>{card.title}</h1>
                     <p className="card-text">{card.text}</p>
@@ -162,22 +163,12 @@ const WhatWeOffer = () => {
             {innData.map((card, index) => (
               <div className="col" key={index}>
                 <div className="card shadow-sm">
-                  <svg
-                    className="bd-placeholder-img card-img-top"
-                    width="100%"
-                    height="225"
-                    xmlns="http://www.w3.org/2000/svg"
-                    role="img"
-                    aria-label="Placeholder: Thumbnail"
-                    preserveAspectRatio="xMidYMid slice"
-                    focusable="false"
-                  >
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#55595c"></rect>
-                    <text x="50%" y="50%" fill="#eceeef" dy=".3em">
-                      Thumbnail
-                    </text>
-                  </svg>
+                  <img
+                    width={360}
+                    height={225}
+                    src={card.img}
+                    alt={card.title}
+                  />
                   <div className="card-body">
                     <h1>{card.title}</h1>
                     <p className="card-text">{card.text}</p>
@@ -203,22 +194,12 @@ const WhatWeOffer = () => {
             {growData.map((card, index) => (
               <div className="col" key={index}>
                 <div className="card shadow-sm">
-                  <svg
-                    className="bd-placeholder-img card-img-top"
-                    width="100%"
-                    height="225"
-                    xmlns="http://www.w3.org/2000/svg"
-                    role="img"
-                    aria-label="Placeholder: Thumbnail"
-                    preserveAspectRatio="xMidYMid slice"
-                    focusable="false"
-                  >
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#55595c"></rect>
-                    <text x="50%" y="50%" fill="#eceeef" dy=".3em">
-                      Thumbnail
-                    </text>
-                  </svg>
+                  <img
+                    width={360}
+                    height={225}
+                    src={card.img}
+                    alt={card.title}
+                  />
                   <div className="card-body">
                     <h1>{card.title}</h1>
                     <p className="card-text">{card.text}</p>
