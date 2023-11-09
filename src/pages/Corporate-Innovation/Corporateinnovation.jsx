9efc21b002d4a762a2079a39bff24123c8513b4a
Ugo Chinememerem <ugofranklin22@gmail.com>
2023-11-09 14:47:31 +0100

    minmalist

--- src/pages/Corporate-Innovation/Corporateinnovation.jsx
+++ src/pages/Corporate-Innovation/Corporateinnovation.jsx
@@ -4,9 +4,12 @@ import { LandingNav } from "../../components/Nav";
 import { Link } from "react-router-dom";
 import { startup } from "../../assets";
 import "./corporate.css";
+
 import Modal from "react-modal";
 import Options from "../../components/Options/Options";
+import { Footer } from "../../components/Footer";
 const Corporateinnovation = () => {
+  const [fixMore, setFixMore] = useState(false);
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const openModal = () => {
@@ -19,25 +22,40 @@ const Corporateinnovation = () => {
   const growData = [
     {
       title: "Start Up Boost",
-      text: "Your offering goes beyond being merely your product or core process. It encompasses more than just a comprehensive set of product or service features and benefits. It involves creating a user and buyer experience that optimally aligns with their rational and emotional needs.",
+      text: "Ubulu Africa specialize in co-founding and nurturing innovative technology-driven startups. We shall invest in you, we shall work for you, and we shall be by your side at every step of the way until you make a scale-up.",
       img: "https://res.cloudinary.com/dtfvdjvyr/image/upload/v1699229169/microsoft-365-TLiWhlDEJwA-unsplash_qdznje.jpg",
       button: "Learn more",
+      path: "/startup",
     },
     {
       title: "Corporate innovation ",
-      text: "It all begins with the realization of who your true client is and understanding their genuine needs. We guide you through the intricacies of today's markets, value chains, buying powers, and processes, helping you discover the often latent needs of your actual buyers and users.",
+      text: "Our mission is to empower organizations to achieve growth, maximize their relevance, and surpass their potential. We collaborate with industry leaders, combining entrepreneurial expertise with corporate influence to create successful ventures and generate new revenue streams.",
       img: "https://res.cloudinary.com/dtfvdjvyr/image/upload/v1699229735/docusign-AKKhZqyLeJ4-unsplash_khesbr.jpg",
       button: "Learn more",
     },
     {
       title: "University-Industry Collaboration ",
-      text: "Enhancing your go-to-market strategy significantly involves making improvements in marketing, sales, and other front-end disciplines within your organization. These are not the easiest areas to change. We leverage all available avenues for change, a matter of critical importance .",
+      text: "At Ubulu Africa, we don't just discuss growth, innovation, and go-to-market strategies; we turn them into reality. Our well-established framework serves as the foundation for creating new ventures and propelling businesses to unparalleled success.",
       img: "https://res.cloudinary.com/dtfvdjvyr/image/upload/v1699229933/paico-oficial-ZDYN7azwO6c-unsplash_fqskz4.jpg",
       button: "Learn more",
+      path: "/university",
     },
 
     // Add more card data objects as needed
   ];
+  // Create an array of state values for each card
+  const [readMoreStates, setReadMoreStates] = useState(
+    growData.map(() => false)
+  );
+
+  const toggleReadMore = (index) => {
+    // Create a copy of the state array
+    const newReadMoreStates = [...readMoreStates];
+    // Toggle the state for the clicked card
+    newReadMoreStates[index] = !newReadMoreStates[index];
+    // Update the state array
+    setReadMoreStates(newReadMoreStates);
+  };
   const ctaText =
     "Join the Ubulu Africa community and be part of our mission to help organizations realize growth, become and remain the most relevant organization they can be, and even surpass that. Our team of hands-on entrepreneurs collaborates with industry leaders to harness their corporate influence to create successful ventures and generate new revenue streams.";
   return (
@@ -53,8 +71,6 @@ const Corporateinnovation = () => {
           <div className={styles.wrapper}>
             <div className={styles.hero_content}>
               <div className={styles.content}>
-                {/* <h1>Ubulu Africa</h1> */}
-
                 <h2> We Ignite Innovation in Corporations</h2>
                 <p>
                   It is our mission to help organizations realize growth, become
@@ -74,52 +90,71 @@ const Corporateinnovation = () => {
             </div>
           </div>
         </div>
-        <br />
-        <br />
-        <br />
-        <center>
-          <div className="container">
-            <h2 style={{ color: "#242436" }}>
-              Ubulu Africa Corporate Innovation Programs
-            </h2>
-          </div>
-        </center>
 
-        <br />
-        <br />
+        <section style={{ backgroundColor: "#F7F8F8" }}>
+          <br />
+          {/* <br />
+          <br /> */}
+          {/* <center>
+            <div className="container">
+              <h2 style={{ color: "#242436" }}>Our Services</h2>
+            </div>
+          </center> */}
 
-        <div>
-          <div className="container">
-            <div className="row">
-              <br />
+          <br />
+          <br />
+
+          <div>
+            <div className="container">
+              <div className="row">
+                <br />
+              </div>
             </div>
           </div>
-        </div>
-        <br />
-        <br />
-        <section className="container">
-          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-            {growData.map((card, index) => (
-              <div className="col" key={index}>
-                <div className="card shadow-sm">
-                  <img
-                    width={360}
-                    height={225}
-                    src={card.img}
-                    alt={card.title}
-                  />
-                  <div className="card-body">
-                    <h1>{card.title}</h1>
-                    <p className="card-text">{card.text}</p>
-                    <button>{card.button}</button>
-                    <div className="d-flex justify-content-between align-items-center"></div>
+          <br />
+          <br />
+          <section className="container">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              {growData.map((card, index) => (
+                <div className="col" key={index}>
+                  <div className="card shadow-sm">
+                    <img
+                      width={360}
+                      height={225}
+                      src={card.img}
+                      alt={card.title}
+                    />
+                    <div className="card-body">
+                      <h1>{card.title}</h1>
+                      <p>
+                        {readMoreStates[index]
+                          ? card.text
+                          : `${card.text.substring(0, 100)}...`}
+                        <span
+                          className="pad"
+                          onClick={() => toggleReadMore(index)}
+                        >
+                          {readMoreStates[index] ? "show less" : "read more"}
+                        </span>
+                      </p>
+                      <Link to={card.path}>
+                        <button>{card.button}</button>
+                      </Link>
+                      <div className="d-flex justify-content-between align-items-center"></div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>
+          </section>
+          <br />
+          <br />
+          <br />
+          <br />
         </section>
       </main>
+
+      <Footer />
     </>
   );
 };
