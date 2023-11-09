304c2a1556ac73467e74d9d2dbd3b9ff4f6a9ae0
Ugo Chinememerem <ugofranklin22@gmail.com>
2023-11-09 12:51:13 +0100

    fixes

--- src/pages/Corporate-Innovation/Corporateinnovation.jsx
+++ src/pages/Corporate-Innovation/Corporateinnovation.jsx
@@ -3,6 +3,7 @@ import styles from "../ci/styles.module.css";
 import { LandingNav } from "../../components/Nav";
 import { Link } from "react-router-dom";
 import { startup } from "../../assets";
+import "./corporate.css";
 import Modal from "react-modal";
 import Options from "../../components/Options/Options";
 const Corporateinnovation = () => {
@@ -15,6 +16,28 @@ const Corporateinnovation = () => {
   const closeModal = () => {
     setModalIsOpen(false);
   };
+  const growData = [
+    {
+      title: "Start Up Boost",
+      text: "Your offering goes beyond being merely your product or core process. It encompasses more than just a comprehensive set of product or service features and benefits. It involves creating a user and buyer experience that optimally aligns with their rational and emotional needs.",
+      img: "https://res.cloudinary.com/dtfvdjvyr/image/upload/v1699229169/microsoft-365-TLiWhlDEJwA-unsplash_qdznje.jpg",
+      button: "Learn more",
+    },
+    {
+      title: "Corporate innovation ",
+      text: "It all begins with the realization of who your true client is and understanding their genuine needs. We guide you through the intricacies of today's markets, value chains, buying powers, and processes, helping you discover the often latent needs of your actual buyers and users.",
+      img: "https://res.cloudinary.com/dtfvdjvyr/image/upload/v1699229735/docusign-AKKhZqyLeJ4-unsplash_khesbr.jpg",
+      button: "Learn more",
+    },
+    {
+      title: "University-Industry Collaboration ",
+      text: "Enhancing your go-to-market strategy significantly involves making improvements in marketing, sales, and other front-end disciplines within your organization. These are not the easiest areas to change. We leverage all available avenues for change, a matter of critical importance .",
+      img: "https://res.cloudinary.com/dtfvdjvyr/image/upload/v1699229933/paico-oficial-ZDYN7azwO6c-unsplash_fqskz4.jpg",
+      button: "Learn more",
+    },
+
+    // Add more card data objects as needed
+  ];
   const ctaText =
     "Join the Ubulu Africa community and be part of our mission to help organizations realize growth, become and remain the most relevant organization they can be, and even surpass that. Our team of hands-on entrepreneurs collaborates with industry leaders to harness their corporate influence to create successful ventures and generate new revenue streams.";
   return (
@@ -54,7 +77,6 @@ const Corporateinnovation = () => {
         <br />
         <br />
         <br />
-        <br />
         <center>
           <div className="container">
             <h2 style={{ color: "#242436" }}>
@@ -65,148 +87,38 @@ const Corporateinnovation = () => {
 
         <br />
         <br />
-        <br />
-        <br />
-        <div>
-          <div className="container">
-            <div className="row">
-              <h2 style={{ color: "#242436" }}>
-                Executive Insights That Transform
-              </h2>
-              <br />
-              <br />
-              <br />
-              <p style={{ color: "#9E9E9E" }}>
-                Discover the cutting-edge trends, powerfully efficient growth
-                strategies, and precision tools perfectly tailored for your
-                company's needs. Immerse yourself in a bespoke and
-                results-driven session brimming with practical insights curated
-                exclusively for your industry. Ubulu Africa's leadership team
-                will guide you through an intensive 2-hour session, complete
-                with an interactive Q&A. We'll also bring in experts from our
-                worldwide network of entrepreneurs and mentors for an in-depth
-                exploration of the Nigeria-African Startup Ecosystem and the
-                dynamic landscape of VC trends. Join us to supercharge your path
-                to success!
-              </p>
-            </div>
-          </div>
-        </div>
-        <br />
-        <br />
-
-        <div style={{ backgroundColor: "#F7F8F8" }}>
-          <br />
-          <div className="container">
-            <div className="row  col-lg-12">
-              <h2 style={{ color: "#242436" }}>Excubation</h2>
-              <br />
-
-              <br />
-              <br />
-              <p style={{ color: "#9E9E9E" }}>
-                Corporate partners can accelerate the development of an MVP or
-                internal team through our flagship startup development program.
-                Over three months of mentor support, events, and
-                experimentation, your product and team will develop at the speed
-                of a startup, making them better suited to make an impact in
-                your company.
-              </p>
-            </div>
-          </div>
-          <br />
-        </div>
-        <br />
-        <br />
-        <br />
-        <br />
 
         <div>
           <div className="container">
             <div className="row">
-              <h2 style={{ color: "#242436" }}>Corporate Venturing</h2>
               <br />
-              <br />
-              <br />
-              <p style={{ color: "#9E9E9E" }}>
-                Corporate partners can engage startups in the Ubulu Africa
-                startup development program for corporate innovation which opens
-                up opportunities for a long-term strategic partnerships (e.g.,
-                acquisition, franchising/licensing the startup’s technology,
-                etc.). You will have the chance to engage with, learn from, and
-                provide expertise to our startup companies in a mutually
-                beneficial engagement.
-              </p>
             </div>
           </div>
         </div>
         <br />
         <br />
-        <br />
-        <br />
-        <div style={{ backgroundColor: "#F7F8F8" }}>
-          <br />
-          <div className="container">
-            <div className="row  col-lg-12">
-              <h2 style={{ color: "#242436" }}>Pilot Acceleration </h2>
-              <br />
-
-              <br />
-              <br />
-              <p style={{ color: "#9E9E9E" }}>
-                Are you looking to tap into world-leading innovation to address
-                your business pain points? Look no further. At Ubulu Africa, we
-                empower corporate partners by addressing their strategic
-                challenges and goals in a game-changing way.
-              </p>
-              <br />
-
-              <br />
-              <br />
-              <h2 style={{ color: "#363624", fontSize: "1.3rem" }}>
-                Here's how it works:{" "}
-              </h2>
-
-              <br />
-              <br />
-              <p style={{ color: "#9E9E9E" }}>
-                Corporate partners present us with three critical challenges or
-                objectives. Our expert team leverages our extensive global
-                startup network to curate a meticulously vetted shortlist of
-                startups. These startups offer groundbreaking platforms,
-                products, or services with the potential to directly address
-                your specific issues.
-              </p>
-              <br />
-              <br />
-              <h2 style={{ color: "#363624", fontSize: "1.3rem" }}>
-                But that's just the beginning:{" "}
-              </h2>
-
-              <br />
-              <br />
-              <p style={{ color: "#9E9E9E" }}>
-                Finalist candidates have the opportunity to pitch their
-                innovative solutions directly to your corporate management. You
-                have the power to select 1-3 winners to embark on collaborative
-                pilot projects. It's time to transform your business through
-                innovation – the future is here.
-              </p>
-            </div>
+        <section className="container">
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            {growData.map((card, index) => (
+              <div className="col" key={index}>
+                <div className="card shadow-sm">
+                  <img
+                    width={360}
+                    height={225}
+                    src={card.img}
+                    alt={card.title}
+                  />
+                  <div className="card-body">
+                    <h1>{card.title}</h1>
+                    <p className="card-text">{card.text}</p>
+                    <button>{card.button}</button>
+                    <div className="d-flex justify-content-between align-items-center"></div>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
-          <br />
-        </div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <section className="secop">
-          <center>
-            <Options />
-          </center>
         </section>
-        <br />
-        <br />
       </main>
     </>
   );
