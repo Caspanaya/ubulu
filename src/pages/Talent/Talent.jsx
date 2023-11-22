7068429633d0232a948c6f4ffb7e6e2a0d3f1072
Ugo Chinememerem <ugofranklin22@gmail.com>
2023-11-22 11:19:12 +0100

    community_cfix

--- src/pages/Talent/Talent.jsx
+++ src/pages/Talent/Talent.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import styles from "../ci/styles.module.css";
 import { startup } from "../../assets";
-import Community from "../../components/CommunityForm/Community";
+import "./Talent.css";
 import { LandingNav } from "../../components/Nav";
 import { Footer } from "../../components/Footer";
 const Talent = () => {
+  const formUrl = () => {
+    const url = "https://forms.gle/XA3XJfoHUUgoTDUW9";
+    // Open the URL in a new tab
+    window.open(url, "_blank");
+  };
+
   return (
     <>
       <LandingNav />
@@ -17,43 +23,23 @@ const Talent = () => {
           <div className={styles.wrapper}>
             <div className={styles.hero_content}>
               <div className={styles.content}>
-                {/* <h1>Ubulu Africa</h1> */}
-
-                <h2> Join the Ubulu Africa community</h2>
+                <h2>Ubulu Africa Community</h2>
                 <p>
-                  Ubulu Africa stands as a beacon of unity, bringing together an
-                  extraordinary tapestry of African startups, global
-                  corporations, universities, innovative social enterprises, and
-                  visionary investors.
+                  Welcome to the Ubulu Africa Community, where we bring together
+                  corporations in pursuit of innovation, aspiring innovators and
+                  startups on a quest for acceleration, and venture capitalists
+                  eager to connect with exceptional startups. Our vibrant
+                  community is meticulously crafted to provide you with a
+                  tailored space for learning and flourishing on your unique
+                  journey.
                 </p>
+                <button className="talent_btn" onClick={formUrl}>
+                  Join Us
+                </button>
               </div>
             </div>
           </div>
         </div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <div className="container">
-          <center>
-            {" "}
-            <h2 style={{ fontSize: "45px", fontWeight: "700" }}>
-              Want to Join Our Community?
-            </h2>
-            <p>
-              Apply today to become a member of our exclusive global network
-            </p>
-          </center>
-          <br />
-          <br />
-        </div>
-        <div>
-          <Community />
-        </div>
-        <br />
-        <br />
-        <br />
-        <br />
       </main>
       <Footer />
     </>
